1ddbc0c08861fbb6f692098751c63a184e1bdc39
Levi Siebra <31908348+Levils114@users.noreply.github.com>
2020-04-08 07:08:55 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
+
+
 
 app.use(express.json());
 app.use(cors());
@@ -11,23 +12,72 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  	const {title} = request.query;
+
+    const results = title ? projects.filter(repositore => repositore.title.includes(title)) : repositories;
+
+    return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,url,techs} = request.body;
+
+  const repositore = {id: uuid(), title, url, techs, likes: 0};
+  repositories.push(repositore);
+
+  return response.json(repositore);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id, likes} = request.params;
+  const {title, url, techs} = request.body;
+
+  const repositoreIndex = repositories.findIndex(repositore => repositore.id === id);
+
+   if (repositoreIndex < 0){
+        return response.status(400).json({error : "mermão, tem esse id não"});
+    } 
+
+   const repositore = {id, title, url, techs, likes: 0};
+
+   repositories[repositoreIndex] = repositore;
+
+   return (response.json(repositore));
+   
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+    const {id} = request.params;
+
+    const repositoreIndex = repositories.findIndex(repositore => repositore.id === id);
+
+    if (repositoreIndex < 0){
+        return response.status(400).json({error : "mermão, tem esse id não"});
+    } 
+
+    repositories.splice(repositoreIndex, 1);
+
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+
+  const {id} = request.params;	
+  
+  const repositoreIndex = repositories.findIndex(repositore => repositore.id === id);
+
+  if (repositoreIndex < 0){
+        return response.status(400).json({error : "mermão, tem esse id não"});
+    }; 
+
+  repositories[repositoreIndex].likes += 1;
+
+  return(response.json(repositories[repositoreIndex])); 
+
+
 });
 
 module.exports = app;
+
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
-const app = require("./app");
+const app = require("./app.js");
 
-app.listen(3333);
+app.listen('3333', () => {
+	console.log('O Servidor está rodando na porta 3333');
+});
